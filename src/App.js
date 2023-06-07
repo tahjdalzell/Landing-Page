0695c441815f0fff7400bc0c82dff5f94687e30f
tahjdalzell <moteltwentyfour@gmail.com>
2023-06-07 14:18:48 -0700

    lading page done

--- src/App.js
+++ src/App.js
@@ -5,20 +5,30 @@ const App = () => {
   return (
     <div className="container">
       <header className="header">
-        <img className="logo" src="logo.png" alt="Logo" />
+        <img className="logo" src="https://minion-dev.s3.amazonaws.com/assets/themes/start_a_career/img/start-a-career-blue-logo-desktop.svg" alt="Logo" />
       </header>
+      <div className="blue-bar">
+        <h2 className="blue-bar-text">Discover the Right Fit</h2>
+      </div>
       <div className="body">
         <div className="content">
-          <img className="picture" src="picture.png" alt="Picture" />
+          <h1 className="header-text">Start Working Remote Today</h1>
+          <img className="picture" src="https://www.realsimple.com/thmb/FY7HYd6A6iRERlEOJzht8Vs8_GE=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/work-from-outdoors-tips-2000-1c4ee7641fd84a42a7878dcb2a1b62fe.jpg" alt="Picture" />
           <p className="paragraph">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum in convallis lorem. Curabitur iaculis
-            molestie velit, vitae finibus elit vestibulum ac. Quisque posuere, tortor et cursus fermentum, ex nulla
-            laoreet ipsum, at condimentum mauris nisi vel erat.
+            Whether you're looking for a local opportunity or a chance to work for a company on the other side of the country, Start A Career Today has something for everyone. No more commuting, no more office politics, and no more stress – just fulfilling work that fits around your life.
           </p>
+          <a className="button" href="https://clickrwrd.com/8Bakt3j" target="_blank" rel="noopener noreferrer">
+            Start Now
+          </a>
+          <div className="blue-section">
+            <img className="logo" src="https://minion-dev.s3.amazonaws.com/assets/themes/jobs_ondemand/img/logos/fedex-logo-white.png" alt="Logo 1" />
+            <img className="logo" src="https://minion-dev.s3.amazonaws.com/assets/themes/jobs_ondemand/img/logos/walmart-logo-white.png" alt="Logo 2" />
+            <img className="logo" src="https://minion-dev.s3.amazonaws.com/assets/themes/jobs_ondemand/img/logos/pepsi-logo-white.png"alt="Logo 3" />
+            <a className="button" href="https://clickrwrd.com/8Bakt3j" target="_blank" rel="noopener noreferrer">
+              Find What's Right for You
+            </a>
+          </div>
         </div>
-        <a className="button" href="https://example.com" target="_blank" rel="noopener noreferrer">
-          Start Now
-        </a>
       </div>
       <footer className="footer">
         <a href="/contact">Contact Us</a>
