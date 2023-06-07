bc4e8c2e1e14d36c40295ca55abbbf3c99591813
tahjdalzell <moteltwentyfour@gmail.com>
2023-06-07 12:21:12 -0700

    barebones

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header className="header">
+        <img className="logo" src="logo.png" alt="Logo" />
       </header>
+      <div className="body">
+        <div className="content">
+          <img className="picture" src="picture.png" alt="Picture" />
+          <p className="paragraph">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum in convallis lorem. Curabitur iaculis
+            molestie velit, vitae finibus elit vestibulum ac. Quisque posuere, tortor et cursus fermentum, ex nulla
+            laoreet ipsum, at condimentum mauris nisi vel erat.
+          </p>
+        </div>
+        <a className="button" href="https://example.com" target="_blank" rel="noopener noreferrer">
+          Start Now
+        </a>
+      </div>
+      <footer className="footer">
+        <a href="/contact">Contact Us</a>
+        <a href="/about">About Us</a>
+      </footer>
     </div>
   );
-}
+};
 
 export default App;
